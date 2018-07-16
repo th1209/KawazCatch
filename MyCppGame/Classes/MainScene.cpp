489e28e3802860894167986b86c1e0c81d12e69b
th1209 <hata.toshiki.1214@gmail.com>
2018-07-16 18:07:05 +0900

    フルーツの生成〜キャッチ処理まで

--- MyCppGame/Classes/MainScene.cpp
+++ MyCppGame/Classes/MainScene.cpp
@@ -9,6 +9,9 @@
 
 USING_NS_CC;
 
+const float kFruitTopMargin = 80.0f;
+const int kFruitSpawnRate = 20;
+
 MainScene::MainScene()
 : _player(NULL)
 {
@@ -62,14 +65,87 @@ bool MainScene::init()
         
         // 移動範囲を画面領域内に切り詰める
         auto winSize = Director::getInstance()->getWinSize();
-        auto halfWidth = _player->getContentSize().width * _player->getScaleX() / 2;
-        auto mixX = 0.0f + halfWidth;
-        auto maxX = winSize.width - halfWidth;
-        newPosition = newPosition.getClampPoint(Vec2(mixX, position.y), Vec2(maxX, position.y));
+        newPosition = newPosition.getClampPoint(Vec2(0.0, position.y), Vec2(winSize.width, position.y));
+        // ↓ フルーツの生成範囲とかも考慮する必要があるので、一旦コメントイン
+//        auto winSize = Director::getInstance()->getWinSize();
+//        auto halfWidth = _player->getContentSize().width * _player->getScaleX() / 2;
+//        auto mixX = 0.0f + halfWidth;
+//        auto maxX = winSize.width - halfWidth;
+//        newPosition = newPosition.getClampPoint(Vec2(mixX, position.y), Vec2(maxX, position.y));
         
         _player->setPosition(newPosition);
     };
     director->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
     
+    // 毎フレupdateを呼ぶ
+    this->scheduleUpdate();
+    
     return true;
+}
+
+void MainScene::update(float delta)
+{
+    int random = rand() % kFruitSpawnRate;
+    if (random == 0) {
+        this->addFruit();
+    }
+    
+    for (auto& fruit : _fruits) {
+        Vec2 basketPositon = _player->getPosition() - Vec2(0, -10);
+        Rect boundingBox = fruit->getBoundingBox();
+        bool isHit = boundingBox.containsPoint(basketPositon);
+        if (isHit) {
+            this->catchFruit(fruit);
+        }
+    }
+}
+
+Sprite* MainScene::addFruit()
+{
+    // フルーツをランダム生成
+    int fruitType = rand() % static_cast<int>(FruitType::kCount);
+    std::string fileName = StringUtils::format("fruit%d.png", fruitType);
+    auto fruit = Sprite::create(fileName);
+    if (fruit == NULL) {
+        return NULL;
+    }
+    fruit->setTag(fruitType);
+    
+    // フルーツの初期位置
+    auto winSize = Director::getInstance()->getWinSize();
+    auto fruitSize = fruit->getContentSize();
+    float fruitXPos = rand() % static_cast<int>(winSize.width);
+    fruit->setPosition(Vec2(fruitXPos, winSize.height - kFruitTopMargin));
+    
+    this->addChild(fruit);
+    _fruits.pushBack(fruit);
+    
+    // 落下 & 削除の動きを付ける
+    auto ground = Vec2(fruitXPos, 0);
+    auto fall = MoveTo::create(3, ground);
+    auto remove = CallFuncN::create([this](Node* node) {
+        auto sprite = dynamic_cast<Sprite*>(node);
+        if (sprite == NULL) return;
+        this->removeFruit(sprite);
+    });
+    auto sequence = Sequence::create(fall, remove, NULL);
+    fruit->runAction(sequence);
+    
+    return fruit;
+}
+
+bool MainScene::removeFruit(Sprite* fruit)
+{
+    if (fruit == NULL) return false;
+    if (! _fruits.contains(fruit)) return false;
+    
+    fruit->removeFromParent();
+    _fruits.eraseObject(fruit);
+    return true;
+}
+
+void MainScene::catchFruit(Sprite* fruit)
+{
+    // 一旦フルーツを削除するだけ
+    this->removeFruit(fruit);
 }
--- MyCppGame/Classes/MainScene.hpp
+++ MyCppGame/Classes/MainScene.hpp
@@ -14,12 +14,30 @@ class MainScene : public cocos2d::Layer {
 public:
     static cocos2d::Scene* createScene();
     CREATE_FUNC(MainScene);
-    CC_SYNTHESIZE_RETAIN(cocos2d::Sprite *, _player, Player);
+    CC_SYNTHESIZE_RETAIN(cocos2d::Sprite*, _player, Player);
+    CC_SYNTHESIZE(cocos2d::Vector<cocos2d::Sprite*>, _fruits, Fruits);
+    
+    virtual void update(float delta);
 
 protected:
     MainScene();
     virtual ~MainScene();
     bool init() override;
+    
+private:
+    enum class FruitType
+    {
+        kApple,
+        kGrape,
+        kOrange,
+        kBanana,
+        kCherry,
+        kCount
+    };
+    
+    cocos2d::Sprite* addFruit();
+    bool removeFruit(cocos2d::Sprite* fruit);
+    void catchFruit(cocos2d::Sprite* fruit);
 };
 
 #endif /* MainScene_hpp */
